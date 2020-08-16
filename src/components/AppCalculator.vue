e878016bf89d017e9c39a6eb2439f270dbc3bbee
RevonZev <revonzev@protonmail.com>
2020-08-16 18:23:06 +0700

    Added AppCalculator.vue base/window

--- src/components/AppCalculator.vue
+++ src/components/AppCalculator.vue
@@ -1,9 +1,21 @@
 <template>
-  <div></div>
+  <div id="app-calculator"></div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+#app-calculator {
+  position: absolute;
+  transform-origin: center;
+  top: calc(50% - (475px / 2));
+  left: calc(50% - (340px / 2));
+  width: 340px;
+  height: 475px;
+  border-radius: 10px;
+  background: #2e282a;
+  box-shadow: -10px 10px 10px rgba(32, 36, 39, 0.63);
+}
+</style>
