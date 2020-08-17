1c5b01458625ff2590d4291e8bbe991a3ee1c81e
RevonZev <revonzev@protonmail.com>
2020-08-17 17:31:00 +0700

    Edited

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/web-calculator/" : "/",
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/web-calculator/'
+    : '/'
+}
