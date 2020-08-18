9dfb18d8b7f1eeba93120fd89f77a351196ad024
RevonZev <revonzev@protonmail.com>
2020-08-18 16:19:06 +0700

    ✨ On load fade-in transition

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
-  <div>
-    <app-background></app-background>
-    <app-calculator></app-calculator>
-    <copyright></copyright>
-  </div>
+  <transition name="fade" appear>
+    <div>
+      <app-background></app-background>
+      <app-calculator></app-calculator>
+      <copyright></copyright>
+    </div>
+  </transition>
 </template>
 
 <script>
 import AppBackground from "./components/AppBackground";
 import AppCalculator from "./components/AppCalculator";
-import Copyright from './components/Copyright'
+import Copyright from "./components/Copyright";
 
 export default {
   name: "App",
   components: {
     "app-background": AppBackground,
     "app-calculator": AppCalculator,
-    "copyright": Copyright,
+    copyright: Copyright,
   },
 };
 </script>
@@ -25,5 +27,15 @@ export default {
 body {
   font-family: "Roboto Condensed";
   margin: 0;
+}
+</style>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
